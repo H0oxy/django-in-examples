c91bed7b07230a44811dee4d496a9592cc59fb9a
H0oxy <xMolod0yx.Arseny@yandex.ru>
2022-09-02 22:21:12 +0500

    markdown

--- ex.1/mysite/blog/templatetags/blog_tags.py
+++ ex.1/mysite/blog/templatetags/blog_tags.py
@@ -1,6 +1,9 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
+
 
 register = template.Library()
 
@@ -19,6 +22,11 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
 
 
 
