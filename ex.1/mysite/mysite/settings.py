b7e13e5706bfdd7ca299dcf36d7cf32238722fa7
H0oxy <xMolod0yx.Arseny@yandex.ru>
2022-09-04 20:19:29 +0500

    -

--- ex.1/mysite/mysite/settings.py
+++ ex.1/mysite/mysite/settings.py
@@ -82,6 +82,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'admin',
+#         'PASSWORD': '123',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
