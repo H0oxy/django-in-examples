3461e7368c74956cb0ce99c32549c2c828b90b98
H0oxy <xMolod0yx.Arseny@yandex.ru>
2022-09-04 20:59:55 +0500

    tilt

--- ex.2/bookmarks/bookmarks/settings.py
+++ ex.2/bookmarks/bookmarks/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
 
 # Application definition
 
@@ -123,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
