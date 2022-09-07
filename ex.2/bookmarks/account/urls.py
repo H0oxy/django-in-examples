7ebdca8be430b7ed3ec05f7121dbd24fe7af0722
H0oxy <xMolod0yx.Arseny@yandex.ru>
2022-09-07 20:11:54 +0500

    edit-profile

--- ex.2/bookmarks/account/urls.py
+++ ex.2/bookmarks/account/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
+
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
@@ -20,5 +21,6 @@ urlpatterns = [
 
     path('register/', views.register, name='register'),
 
-
+    path('edit/', views.edit, name='edit'),
 ]
+
