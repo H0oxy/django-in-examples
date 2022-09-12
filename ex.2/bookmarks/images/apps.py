117163c7186af76ffbd2b7872d5bfab8941349a3
H0oxy <xMolod0yx.Arseny@yandex.ru>
2022-09-12 19:01:05 +0500

    signals

--- ex.2/bookmarks/images/apps.py
+++ ex.2/bookmarks/images/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from django.db.models import Count
 
 
 class ImagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'images'
+
+
+def ready(self):
+    import images.signals
