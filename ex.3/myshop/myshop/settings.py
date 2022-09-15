dabfc5ff1cdbc51d6bf4280408a4015164a8d16c
H0oxy <xMolod0yx.Arseny@yandex.ru>
2022-09-15 19:31:48 +0500

    checkout

--- ex.3/myshop/myshop/settings.py
+++ ex.3/myshop/myshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'cart',
     'shop',
+    'orders',
 
 ]
 
@@ -67,6 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'cart.context_processors.cart',
             ],
         },
     },
