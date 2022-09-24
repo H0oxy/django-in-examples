aa4aac49068fd68d85c17c47b7a1d3663fd85dad
H0oxy <xMolod0yx.Arseny@yandex.ru>
2022-09-24 13:23:37 +0500

    different-types-content

--- ex.4/educa/educa/settings.py
+++ ex.4/educa/educa/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'students',
+    'embed_video',
 
 ]
 
@@ -123,7 +124,16 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.urls import reverse_lazy
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
+
+
--- ex.4/educa/students/views.py
+++ ex.4/educa/students/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, FormView
 from .forms import CourseEnrollForm
+from django.views.generic.list import ListView
+from courses.models import Course
+from django.views.generic.detail import DetailView
 
 
 class StudentRegistrationView(CreateView):
@@ -32,3 +35,31 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
         return reverse_lazy('student_course_detail', args=[self.course.id])
 
 
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qs = super(StudentCourseListView, self).get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+
+class StudentCourseDetailView(DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qs = super(StudentCourseDetailView, self).get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super(StudentCourseDetailView, self).get_context_data(**kwargs)
+        # Получаем объект курса.
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+        # Получаем текущий модуль по параметрам запроса.
+            context['module'] = course.modules.get(id=self.kwargs['module_id'])
+        else:
+        # Получаем первый модуль.
+            context['module'] = course.modules.all()[0]
+        return context
